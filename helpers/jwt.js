7ce5bfbae52bc167b53eb8072666afe581c43da6
Liubov Zatsepina <liubov_zatsepina@epam.com>
2024-05-09 19:33:37 +0300

    version 0.6

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,15 +11,11 @@ function authJwt() {
         path: [
             { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/notes(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
+            { url: /\/api\/v1\/notes(.*)/, methods: ['GET', 'OPTIONS', 'POST', 'DELETE'] },
             { url: /\/api\/v1\/projects(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
             { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
-            // { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
-            // { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`,
-            // `${api}/notes/`,
-            // { url: /(.*)/ },
         ]
     });
 }
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,7 @@
-const { Note } = require('../models/note');
 const express = require('express');
-const { User } = require('../models/user');
-const { Comment } = require('../models/comment');
 const router = express.Router();
+const { Note } = require('../models/note');
+const { Comment } = require('../models/comment');
 
 router.get(`/`, async (req, res) =>{
     const noteList = await Note.find();
@@ -86,8 +85,6 @@ router.get('/comments/:noteId', async(req,res)=>{
     if(!comments) {
         res.status(500).json({message: 'The comments in note with the given ID were not found.'})
     }
-
-    
     res.status(200).send(comments);
 })
 
@@ -107,7 +104,7 @@ router.post('/commentCreate', async (req,res)=>{
 })
 
 router.delete('/comments/:commentId', (req, res)=>{
-    Comment.findByIdAndRemove(req.params.commentId).then(comment =>{
+    Comment.findByIdAndDelete(req.params.commentId).then(comment =>{
         if(comment) {
             return res.status(200).json({success: true, message: 'the comment is deleted!'})
         } else {
@@ -117,24 +114,5 @@ router.delete('/comments/:commentId', (req, res)=>{
        return res.status(500).json({success: false, error: err}) 
     })
 })
-
-// router.get('/usernotes/:userId', async(req,res)=>{
-//     const user = await User.findById(req.params.userId).select('notes');
-
-//     console.log(user);
-
-//     if(!user) {
-//         res.status(500).json({message: 'The user with the given ID was not found.'})
-//     }
-
-//     const notes = await Note.find({ _id: { $in: user.notes } });
-
-//     console.log(notes);
-
-//     if(!notes) {
-//         res.status(500).json({message: 'The notes in user with the given ID were not found.'})
-//     }
-//     res.status(200).send(notes);
-// })
 
 module.exports = router;
